1a5ea6ef128271f452430eec5a5dd45a6d5e1b12
ManshulVBelani <manshulvbelani@gmail.com>
2015-08-24 12:48:16 +0530

    reporting multiple selection for excel version below 2010 and modified to prevent reporting mode twice for the first time

--- source/appModules/excel.py
+++ source/appModules/excel.py
@@ -9,20 +9,30 @@ import controlTypes
 import api
 import speech
 import winUser
-import ui                     
+import ui                    
 
 class AppModule(appModuleHandler.AppModule):
         
-    def event_nameChange(self, obj, nextHandler):
+    def event_stateChange(self, obj, nextHandler):
         if obj.name== "Selection Mode Extend Selection" or obj.name== "Selection Mode Add to Selection":
-            ui.message(obj.name)
             return
         nextHandler()
         
-    def event_stateChange(self, obj, nextHandler):
+    def event_nameChange(self, obj, nextHandler):
+        #to report selection mode in version 2010 and above
         if obj.name== "Selection Mode Extend Selection" or obj.name== "Selection Mode Add to Selection":
             ui.message(obj.name)
             return
+        
+        #to report selection mode in version below 2010
+        elif obj.value== "Extend Selection" or obj.value== "Add to Selection":
+            ui.message(obj.name + obj.value)
+            return
         nextHandler()
-
- 
+        
+    #to report selection mode in version below 2010
+    def event_valueChange(self, obj, nextHandler):
+        if obj.value== "Extend Selection" or obj.value== "Add to Selection":
+            ui.message(obj.name + obj.value)
+            return
+        nextHandler()
